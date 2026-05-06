3ae4242c7e2b03203fcb34d278e9add892dd8f4d
Aziz Shameem <81754147+Aziz-Shameem@users.noreply.github.com>
2026-05-06 18:15:18 +0530

    Create mtp_naive.py

---
+++ models/mtp_naive.py
@@ -0,0 +1,44 @@
+"""GPT model with Multi-Token Prediction (MTP) - independent multi-horizon prediction from the same state"""
+import math
+from utils import linear, softmax, rmsnorm, Value
+
+def gpt_mtp_naive(token_id, pos_id, keys, values, state_dict, n_layer, n_head, head_dim, heads_per_group, n_tokens):
+    tok_emb = state_dict['wte'][token_id] # token embedding
+    pos_emb = state_dict['wpe'][pos_id] # position embedding
+    x = [t + p for t, p in zip(tok_emb, pos_emb)] # joint token and position embedding
+    # x = [t for t, p in zip(tok_emb, pos_emb)] # not adding PE
+    x = rmsnorm(x) # note: not redundant due to backward pass via the residual connection
+
+    for li in range(n_layer):
+        # 1) Multi-head Attention block
+        x_residual = x
+        x = rmsnorm(x)
+        q = linear(x, state_dict[f'layer{li}.attn_wq'])
+        k = linear(x, state_dict[f'layer{li}.attn_wk'])
+        v = linear(x, state_dict[f'layer{li}.attn_wv'])
+        keys[li].append(k)
+        values[li].append(v)
+        x_attn = []
+        for h in range(n_head):
+            hs = h * head_dim
+            hs_kv = (h // heads_per_group) * head_dim
+            q_h = q[hs:hs+head_dim]
+            k_h = [ki[hs_kv:hs_kv+head_dim] for ki in keys[li]]
+            v_h = [vi[hs_kv:hs_kv+head_dim] for vi in values[li]]
+            attn_logits = [sum(q_h[j] * k_h[t][j] for j in range(head_dim)) / head_dim**0.5 for t in range(len(k_h))]
+            attn_weights = softmax(attn_logits)
+            head_out = [sum(attn_weights[t] * v_h[t][j] for t in range(len(v_h))) for j in range(head_dim)]
+            x_attn.extend(head_out)
+        x = linear(x_attn, state_dict[f'layer{li}.attn_wo'])
+        x = [a + b for a, b in zip(x, x_residual)]
+        # 2) MLP block
+        x_residual = x
+        x = rmsnorm(x)
+        x = linear(x, state_dict[f'layer{li}.mlp_fc1'])
+        x = [xi.relu() for xi in x]
+        x = linear(x, state_dict[f'layer{li}.mlp_fc2'])
+        x = [a + b for a, b in zip(x, x_residual)]
+
+    logits = linear(x, state_dict['lm_head'])
+    logits_additional = [linear(x, state_dict[f'lm_head{i}']) for i in range(2, n_tokens + 1)] # additional heads for MTP
+    return [logits] + logits_additional
